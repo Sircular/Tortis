7f66d74fdbed46bbfc889f9d088305c0bf1c3b06
Walter Mays <sircular17@gmail.com>
2017-09-25 15:31:38 -0500

    Implemented the beginnings of a proper game loop.

--- tetris.c
+++ tetris.c
@@ -11,6 +11,7 @@
 #include <sys/time.h>
 
 #include "board.h"
+#include "color.h"
 
 #define BOARD_WIDTH 10
 #define BOARD_HEIGHT 20
@@ -20,32 +21,48 @@ static Board* board;
 static WINDOW* gameWin;
 static WINDOW* boardWin;
 
+/* Initializes the game window and board window. */
+void initWindows(WINDOW** gameWinPtr, WINDOW** boardWinPtr);
+
 int main() {
+    // set up the screen
     initscr();
     raw();
     noecho();
+    curs_set(0);
     //nodelay(stdscr, true);
     refresh();
+    colors_init();
+    board = board_init(BOARD_WIDTH, BOARD_HEIGHT);
+    // initialize the windows
+    initWindows(&gameWin, &boardWin); 
 
-    {
-        int boardWinWidth = (BOARD_WIDTH*2) + 2;
-        int winHeight = BOARD_HEIGHT+2;
-        int totalWidth = boardWinWidth+BOARD_WIDTH;
-        board = board_init(BOARD_WIDTH, BOARD_HEIGHT);
-        gameWin = newwin(winHeight, totalWidth, 0, 0);
-        boardWin = derwin(gameWin, winHeight, boardWinWidth, 0, 0);
+    // game loop
+    bool running = true;
+    while (running) {
+        board_draw(board, boardWin);
+        wrefresh(gameWin);
+        refresh();
+        if (getch() == 10) {
+            running = false;
+        }
     }
-
-    int i;
-    for (i = 0; i < BOARD_WIDTH; i++) {
-        board->board[COORD_INDEX(board, i, BOARD_HEIGHT-1)] = 1;
-    }
-    board_draw(board, boardWin);
-    wrefresh(gameWin);
-    refresh();
-    getch();
 
     board_free(board);
     endwin();
     return 0;
+}
+
+void initWindows(WINDOW** gameWinPtr, WINDOW** boardWinPtr) {
+    int boardWinWidth = (BOARD_WIDTH*2) + 2;
+    int winHeight = BOARD_HEIGHT+2;
+    int totalWidth = boardWinWidth+BOARD_WIDTH;
+
+    int tWidth, tHeight;
+    getmaxyx(stdscr, tHeight, tWidth);
+
+    board = board_init(BOARD_WIDTH, BOARD_HEIGHT);
+    *gameWinPtr = newwin(winHeight, totalWidth, (tHeight-winHeight)/2,
+            (tWidth-totalWidth)/2);
+    *boardWinPtr = derwin(*gameWinPtr, winHeight, boardWinWidth, 0, 0);
 }
